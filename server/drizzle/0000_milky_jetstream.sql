b56a44d6a7dbb9273b6835134ac9aa1b29198ead
faseeh1279 <faseehraza1279@gmail.com>
2026-06-03 19:43:23 +0500

    Removed the Server and also deleted the unnecessary node_modules folder from the root directory.

--- server/drizzle/0000_milky_jetstream.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "cars" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"make" varchar(256) NOT NULL,
-	"model" varchar(256) NOT NULL,
-	"year" integer NOT NULL,
-	"price" numeric(10, 2) NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL
-);
